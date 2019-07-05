f06e6a094fd0229ae997fff0d75f77e0323bdd1a
TechnicPlay <33784577+TechnicPlay@users.noreply.github.com>
2019-07-05 15:14:20 +0200

    Initial Commit

---
+++ CryptMethods/AES.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataEncrypter.CryptMethods
+{
+    public static class AES
+    {
+        public static IEnumerable<byte> Encrypt(IEnumerable<byte> plaintext, IEnumerable<byte> cypher)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<byte> Decrypt(IEnumerable<byte> cyphertext, IEnumerable<byte> cypher)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void KeySchedule()
+        {
+
+        }
+
+        private static SubBytes
+    }
+}
